7bcd546191af007d7664b40b6ff09619ebee5a4f
Ogechukwu Sylvia Okwu <impeccablesylvia@gmail.com>
2022-07-13 16:01:49 +0000

    Attempt challenge 1 &2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,16 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
+    let sum = 0;
+    for (const val of arrayOfNumbers){
+      sum += val  
+      
+    }
+    return sum
     return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ 
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +35,12 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
+  var arrayOfNumbers = arrayOfNumbers.filter(num => {
+    return num % 2 == 0;
+  });
     return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -56,4 +66,3 @@ function celsiusToFahrenheit(arrayOfNumbers) {
 // DO NOT EDIT THIS BLOCK
 module.exports = assignment;
 // ========================
-
